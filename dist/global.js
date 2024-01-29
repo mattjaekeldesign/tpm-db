8a4881e081c979e91f3ee480ef1b7c571f86671b
Matt Jaekel <mattjaekeldesign@gmail.com>
2024-01-29 16:11:12 -0500

    Ok

---
+++ dist/global.js
@@ -0,0 +1,1 @@
+!function(e,o){"object"==typeof exports&&"object"==typeof module?module.exports={}:"function"==typeof define&&define.amd?define("global",[],o):"object"==typeof exports?exports.global={}:e.global={}}(this,(()=>({})));
---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    ["global"]: "./src/index.js",
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+    library: "[name]",
+    libraryTarget: "umd",
+    globalObject: "this",
+    umdNamedDefine: true,
+    clean: true,
+  },
+};
