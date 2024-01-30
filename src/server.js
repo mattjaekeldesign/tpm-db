e117abdb77f78a901bc3e2c1bd3b31b29772d905
Matt Jaekel <mattjaekeldesign@gmail.com>
2024-01-30 15:15:48 -0500

    env url

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 const app = express();
 const port = 3000;
 
-const API_ENDPOINT = 'https://nam04.safelinks.protection.outlook.com/?url=https%3A%2F%2Fapitpm.azurewebsites.net%2Fswagger%2Findex.html&data=05%7C02%7Cmjaekel%40Maintech.com%7C9d03f28133604fe8d14508dc0c7e3b59%7Ce46f931e83c94e84a13a55e173ea6039%7C0%7C0%7C638398984004150041%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C3000%7C%7C%7C&sdata=pArlv9XAtqcLuhD8geQODLeD2sgu0Rto9%2BUs6peE7Bc%3D&reserved=0';
+const API_ENDPOINT = `${process.env.AZURE_API_URL}`;
 const USERNAME = `${process.env.AZURE_API_USERNAME}`;
 const PASSWORD = `${process.env.AZURE_API_PASSWORD}`;
 
